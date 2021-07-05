865632e5d1c7022aedb2d0ae4118585ac3b9d74e
Lameck1 <lameck72@gmail.com>
2021-07-05 15:59:39 +0300

    Add #removeBook method

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,10 @@
 const books = [];
 
-const  addBook = (name, author) => {
-  const book = {name, author}
-  books.push(book)
+const addBook = (name, author) => {
+  const book = { name, author };
+  books.push(book);
+};
+
+const removeBook = (name) => {
+  books.filter((book) => book.name !== boo)
 }
