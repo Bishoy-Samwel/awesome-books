f6844684beb214c3f9c2e73a74da458c47a5de51
Lameck1 <lameck72@gmail.com>
2021-07-05 17:26:06 +0300

    Add eventListener to form for submit button

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
-const books = [{title:'title1', author:'author1'}, {title:'title2', author:'author2'}];
+const books = [{ title: 'title1', author: 'author1' }, { title: 'title2', author: 'author2' }];
 
 const addBook = (title, author) => {
   const book = { title, author };
@@ -6,29 +6,43 @@ const addBook = (title, author) => {
 };
 
 const removeBook = (title) => {
-  books.filter((book) => book.title !== title)
-}
+  books.filter((book) => book.title !== title);
+};
 
 // This function receive a book object return book div
 const createBookDiv = (book) => {
-    const bookDiv = document.createElement('div');
-    const title = document.createElement('p');
-    title.textContent = book.title
-    const author = document.createElement('p');
-    author.textContent = book.author
-    const delBtn = document.createElement('button');
-    delBtn.textContent = 'Remove'
-    const hr = document.createElement('hr');
-    bookDiv.append(title,author,delBtn,hr)
-    return bookDiv
-}
+  const bookDiv = document.createElement('div');
+  const title = document.createElement('p');
+  title.textContent = book.title;
+  const author = document.createElement('p');
+  author.textContent = book.author;
+  const delBtn = document.createElement('button');
+  delBtn.textContent = 'Remove';
+  const hr = document.createElement('hr');
+  bookDiv.append(title, author, delBtn, hr);
+  return bookDiv;
+};
 
-//this function add book list 
+// this function add book list
 const showBooks = () => {
   const bookList = document.querySelector('#books-list');
   const booksDiv = document.createElement('div');
-  books.forEach((book) =>   {
-    booksDiv.appendChild(createBookDiv(book))
-  })
-  bookList.appendChild(booksDiv)
-}
+  books.forEach((book) => {
+    booksDiv.appendChild(createBookDiv(book));
+  });
+  bookList.appendChild(booksDiv);
+};
+
+const clearInputs = () => {
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+};
+
+document.querySelector('#book-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  addBook(title, author);
+  showBooks();
+  clearInputs();
+});
